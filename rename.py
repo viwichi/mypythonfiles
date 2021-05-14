e1e1e9f34702b9fd5adb0f4e8f80d6775c471940
viwichi <72896248+viwichi@users.noreply.github.com>
2021-05-14 08:59:38 +0530

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,21 +1,24 @@
 # Python program to rename all file
 # names in your directory 
+# Make sure you have python installed
+# This program doesn't require any other configurations.
+# Just give the path to the directory  in line 10
+# And you'll be good to go
+
 import os
   
-os.chdir('/home/visws/programs/cpp')
+os.chdir('give/path/to/your/directory/here')
 print(os.getcwd())
-COUNT = 1
-  
-# Function to increment count 
-# to make the files sorted.
+INC = 1
+
 def increment():
-    global COUNT
-    COUNT = COUNT + 1
+    global INC
+    INC = INC + 1
   
   
 for f in os.listdir():
     f_name, f_ext = os.path.splitext(f)
-    f_name = "file" + str(COUNT)
+    f_name = "file" + str(INC)
     increment()
   
     new_name = '{} {}'.format(f_name, f_ext)
