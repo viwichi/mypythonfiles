57fc99ab97c87a07ae83e74449220d227fa61f13
viwichi <72896248+viwichi@users.noreply.github.com>
2021-05-14 09:06:57 +0530

    Update sort_files.py

--- sort_files.py
+++ sort_files.py
@@ -1,21 +1,27 @@
+# Make sure you have python3 or up installed 
+# Check out https://www.python.org/downloads/ to download.
+# And you are good to go :)
+# This program does't require any other configuaratios.
+#Make sure you give the right path in lin 12
+
 import os
 import shutil
    
-# Write the name of the directory here,
+# Give the name of the directory here,
 # that needs to get sorted
-path = '/home/visws/Downloads'
+path = '/path/to/your/directory'
   
   
-# This will create a properly organized 
-# list with all the filename that is
-# there in the directory
+# This will create a list with
+# All the filename that directory
+
 list_ = os.listdir(path)
    
-# This will go through each and every file
+# This will go through each file
 for file_ in list_:
     name, ext = os.path.splitext(file_)
   
-    # This is going to store the extension type
+    # This is to store the extension type
     ext = ext[1:]
   
     # This forces the next iteration,
@@ -23,13 +29,14 @@ for file_ in list_:
     if ext == '':
         continue
   
-    # This will move the file to the directory
-    # where the name 'ext' already exists
+    # This is to move the file to the directory
+   
     if os.path.exists(path+'/'+ext):
        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
   
     # This will create a new directory,
-    # if the directory does not already exist
+    # If the directory does not already exist
+    # And files will be moved to the directory
     else:
         os.makedirs(path+'/'+ext)
         shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
